d33cf6c1b657fc0a7800378c366075a677d280fa
rd <nischal.hp@gmail.com>
2014-07-31 23:22:02 +0530

    DB COnnector workin

--- TicketGenie/src/DBConnector.java
+++ TicketGenie/src/DBConnector.java
@@ -1,4 +1,35 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
 
 public class DBConnector {
+
+	public static final String host = "127.0.0.1";
+	public static final String port = "5432";
+	public static final String username = "postgres";
+	public static final String password = "tiger";
+	public static final String db = "postgres";
+
+	public Connection getConnection() {
+		Connection c = null;
+		try {
+			Class.forName("org.postgresql.Driver");
+			String connString = "jdbc:postgresql://" + host + ":" + port + "/"
+					+ db;
+			c = DriverManager.getConnection(connString, username, password);
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.err.println(e.getClass().getName() + ": " + e.getMessage());
+			System.exit(0);
+		}
+		System.out.println("Opened database successfully");
+		return c;
+	}
+
+	public static void main(String args[]) {
+
+		DBConnector obj = new DBConnector();
+		Connection c = obj.getConnection();
+
+	}
 
 }
